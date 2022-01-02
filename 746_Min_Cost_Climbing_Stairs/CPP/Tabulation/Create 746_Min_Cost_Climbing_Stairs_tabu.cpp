7bac4d56b11734ed5dd7364faed2eca783bfecba
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2022-01-03 01:36:14 +0530

    Update Create 746_Min_Cost_Climbing_Stairs_tabu.cpp

--- 746_Min_Cost_Climbing_Stairs/CPP/Tabulation/Create 746_Min_Cost_Climbing_Stairs_tabu.cpp
+++ 746_Min_Cost_Climbing_Stairs/CPP/Tabulation/Create 746_Min_Cost_Climbing_Stairs_tabu.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+
+int minCostClimbingStairs(vector<int>& cost) {
+    
+    int dpSize = cost.size()+2;
+    int dp[dpSize];
+    dp[dpSize-1] = dp[dpSize-2] = 0;
+    
+    for(int i=dpSize-3;i>=0;i--)
+    {
+        dp[i] = cost[i] + min(dp[i+1],dp[i+2]);
+    }
+    
+    return min(dp[0],dp[1]);
+    
+}
+
+};

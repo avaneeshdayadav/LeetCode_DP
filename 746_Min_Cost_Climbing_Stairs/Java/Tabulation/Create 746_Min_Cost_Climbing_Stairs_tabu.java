625a76935d11055338ac04e0125ff89fbc009d77
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2022-01-03 01:30:19 +0530

    Update Create 746_Min_Cost_Climbing_Stairs_tabu.java

--- 746_Min_Cost_Climbing_Stairs/Java/Tabulation/Create 746_Min_Cost_Climbing_Stairs_tabu.java
+++ 746_Min_Cost_Climbing_Stairs/Java/Tabulation/Create 746_Min_Cost_Climbing_Stairs_tabu.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    
+    public int minCostClimbingStairs(int[] cost) {
+        
+        int dpSize = cost.length+2;
+        int dp[] = new int[dpSize];
+        Arrays.fill(dp,-1);
+        
+        
+        dp[dpSize-1] = dp[dpSize-2] = 0;
+        
+        for(int i=dpSize-3;i>=0;i--)
+        {
+            dp[i] = cost[i] + Math.min(dp[i+1],dp[i+2]);
+        }
+        return Math.min(dp[0],dp[1]);
+    }
+    
+}

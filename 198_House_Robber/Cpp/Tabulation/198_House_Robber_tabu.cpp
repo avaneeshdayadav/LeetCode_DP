67463fbfa6e47a2d2cbe39702612db0542581e98
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2022-01-03 12:30:30 +0530

    Update 198_House_Robber_tabu.cpp

--- 198_House_Robber/Cpp/Tabulation/198_House_Robber_tabu.cpp
+++ 198_House_Robber/Cpp/Tabulation/198_House_Robber_tabu.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int dpSize = nums.size()+2;
+        int dp[dpSize];
+        memset(dp, -1, sizeof(dp));
+        
+        dp[dpSize-1] = dp[dpSize-2] = 0;
+        
+        for(int i=dpSize-3;i>=0;i--)
+        {
+            dp[i] = max(nums[i]+dp[i+2],dp[i+1]);
+        }
+        return dp[0];
+    }
+    
+};

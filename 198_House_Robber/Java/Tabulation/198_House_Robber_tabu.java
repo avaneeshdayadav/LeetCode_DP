ea62023262649cca5720f19c8b28a5ec06e115ef
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2022-01-03 12:25:53 +0530

    Update 198_House_Robber_tabu.java

--- 198_House_Robber/Java/Tabulation/198_House_Robber_tabu.java
+++ 198_House_Robber/Java/Tabulation/198_House_Robber_tabu.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public int rob(int[] nums) {
+        
+        int dpSize = nums.length+2;
+        int dp[] = new int[dpSize];
+        
+        Arrays.fill(dp,-1);
+        
+        dp[dpSize-1] = dp[dpSize-2] = 0;
+        
+        for(int i=dpSize-3;i>=0;i--)
+            dp[i] = Math.max(nums[i]+dp[i+2], dp[i+1]);
+        
+        return dp[0];
+    }
+    
+    
+}

5ef384af71c03df5b3d386ab3e3e2063c89abebd
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2022-01-03 12:11:57 +0530

    Update 198_House_Robber_memo.java

--- 198_House_Robber/Java/Memoization/198_House_Robber_memo.java
+++ 198_House_Robber/Java/Memoization/198_House_Robber_memo.java
@@ -1,1 +1,22 @@
 
+class Solution {
+    public int rob(int[] nums) {
+        
+        int memo[] = new int[nums.length];
+        Arrays.fill(memo,-1);
+        
+        return ans(nums,0,memo);
+    }
+    
+    public int ans(int[] nums,int indx,int memo[])
+    {
+        if(indx >= nums.length)
+            return 0;
+        
+        if(memo[indx] != -1)
+            return memo[indx];
+        
+        memo[indx] = Math.max(nums[indx]+ans(nums,indx+2,memo), ans(nums,indx+1,memo));
+        return memo[indx];
+    }
+}

fa805a3cc5cfbfa18b5bf69d20e0724edf159a6a
Avaneesh Yadav <60056707+avaneeshdayadav@users.noreply.github.com>
2022-01-06 19:46:06 +0530

    Create 45_Jump_Game_II_tabu.java

---
+++ 45_Jump_Gam_II/Java/Tabulation/45_Jump_Game_II_tabu.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int jump(int[] nums) {
+     
+        int dp[] = new int[nums.length];
+        dp[nums.length-1] = 0;
+        
+        int need = 1;
+        // int minIndx = nums.length-1;
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            if(nums[i] < need)
+            {
+                need++;
+                dp[i] = 10000;
+                
+            }
+            else
+            {
+                int j = nums[i] + i;
+                if(j>nums.length-1)
+                    j = nums.length-1;
+                // if(j>minIndx)
+                //     j=minIndx;
+                int min = dp[j];
+
+                for(;j>i;j--)
+                {
+                    if(dp[j] < min)
+                    {
+                        // minIndx = j;
+                        min = dp[j];
+                    }
+                        
+                }
+
+                dp[i] = min+1;
+                need=1;
+                
+            }
+            
+        }
+        
+        for(int i=0;i<dp.length;i++)
+            System.out.println(dp[i]);
+        return dp[0];
+    }
+}
